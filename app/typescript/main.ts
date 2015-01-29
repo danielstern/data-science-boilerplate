a81814efb798e4a0c5c03d21ae876f4669553d44
Daniel Stern <daniel@danielstern.ca>
2015-01-28 21:11:54 -0500

    added data n stuff

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -1,6 +1,8 @@
 /// <reference path="definition/d3.d.ts" />
 
 declare var angular:any;
+declare var d3:D3.Base;
+
 
 angular.module("DemoApp",['ui.router'])
 .config(function($stateProvider,$urlRouterProvider) {
@@ -9,7 +11,12 @@ angular.module("DemoApp",['ui.router'])
 	$stateProvider
 	.state('home', {
 		url: '/',
-    	templateUrl:'partials/main.html'
+    	templateUrl:'partials/main.html',
+    	controller:function(){
+    		console.log("What in the name of Science is going on here?");
+        
+
+    	}
 	})
 	.state('usage',{
 		url: '/usage',
